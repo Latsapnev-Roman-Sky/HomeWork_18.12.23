1987c9aae9f42eb85b3edd58065538228e7fbf8b
latsa <latsapnev01@mail.ru>
2023-12-18 20:45:11 +0300

    Task №2.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int clientOS = 0;
 
-        if (clientOS == 3) {
+        if (clientOS == 0) {
 
             System.out.println("Установите версию приложения для iOS по ссылке");
 
@@ -19,6 +19,34 @@ public class Main {
 
         }
 
+        //Task 2
+
+        clientOS = 0;
+        int yearOS = 2015;
+        int clientDeviceYear = 2014;
+
+        if (clientOS == 0 && clientDeviceYear < yearOS) {
+
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 0 && clientDeviceYear >= yearOS) {
+
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 1 && clientDeviceYear < yearOS) {
+
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+        } else if (clientOS == 1 && clientDeviceYear >= yearOS) {
+
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        } else {
+
+            System.out.println("Некорректная ОС");
+
+        }
 
     }
+
 }
