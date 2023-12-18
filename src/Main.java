8bc2bd75cfcf0514c89a29f676fa3d4519d54867
latsa <latsapnev01@mail.ru>
2023-12-18 20:35:25 +0300

    Task №1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Task 1
+
+        int clientOS = 0;
+
+        if (clientOS == 3) {
+
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 1) {
+
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        } else {
+
+            System.out.println("Некорректная ОС");
+
+        }
+
+
     }
 }
